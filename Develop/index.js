cbe0f6b66b4ce09a64616a776f339e788a33c2bc
Aphillip22 <amber.rain.phillips@gmail.com>
2021-09-26 16:44:51 -0600

    add import/export

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,65 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const inquirer = require('inquirer');
+const {writeFile, copyFile} = require('./utils/generateMarkdown');
+const readmeDataArgs = process.argv.slice(2);
+const [fileName, data] = readmeDataArgs;
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'Title',
+      message: 'What is the name of your project?'
+    },
+    {
+        type: 'input',
+        name: 'Description',
+        message: 'What would you like your project description to be?'
+    },
+    {
+        type: 'input',
+        name: 'Installation',
+        message: 'What are the installation instructions?'
+    },
+    {
+        type: 'input',
+        name: 'Usage',
+        message: 'What is the inteded usage of your project?'
+    },
+    {
+        type: 'input',
+        name: 'Contributing',
+        message: 'How can someone contribute to this project?'
+    },
+    {
+        type: 'input',
+        name: 'Tests',
+        message: 'How can someone test this project?'
+    },
+    {
+        type: 'input',
+        name: 'Github Name',
+        message: 'What is your Github username?'
+    },
+    {
+        type: 'list',
+        message: 'What is your preferred method of communication?',
+        name: 'contact',
+        choices: ['email', 'phone', 'Slack', 'smoke signal']
+    },
+    {
+      type: 'checkbox',
+      message: 'What languages are included in this project?',
+      name: 'Technologies',
+      choices: ['HTML', 'CSS', 'JavaScript', 'SQL']
+    }
+  ])
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+fs.writeFile('../readme-guide.md', generatePage(fileName, data), err => {
+    if (err) throw new Error(err);
 
 // TODO: Create a function to initialize app
 function init() {}
