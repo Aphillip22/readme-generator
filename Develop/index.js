2ae0f9391e36483b87c4390eec4de377f20e7ee7
Aphillip22 <amber.rain.phillips@gmail.com>
2021-09-26 18:48:38 -0600

    add license badges and begin markdown

--- Develop/index.js
+++ Develop/index.js
@@ -5,61 +5,92 @@ const {writeFile, copyFile} = require('./utils/generateMarkdown');
 const readmeDataArgs = process.argv.slice(2);
 const [fileName, data] = readmeDataArgs;
 // TODO: Create an array of questions for user input
-inquirer
-  .prompt([
+const questions = [
     {
       type: 'input',
-      name: 'Title',
+      name: 'title',
       message: 'What is the name of your project?'
     },
     {
         type: 'input',
-        name: 'Description',
+        name: 'description',
         message: 'What would you like your project description to be?'
     },
     {
         type: 'input',
-        name: 'Installation',
+        name: 'installation',
         message: 'What are the installation instructions?'
     },
     {
         type: 'input',
-        name: 'Usage',
+        name: 'usage',
         message: 'What is the inteded usage of your project?'
     },
     {
+        type: 'checkbox',
+        name: 'license',
+        message: 'Which licenses would you like to include?',
+        choices: ['MIT', 'Apache', 'BSD'],
+    },
+    {
         type: 'input',
-        name: 'Contributing',
+        name: 'contributing',
         message: 'How can someone contribute to this project?'
     },
     {
         type: 'input',
-        name: 'Tests',
+        name: 'tests',
         message: 'How can someone test this project?'
     },
     {
         type: 'input',
-        name: 'Github Name',
+        name: 'username',
         message: 'What is your Github username?'
     },
     {
         type: 'input',
-        name: 'Email',
+        name: 'email',
         message: 'What is your email address?',
     },
     {
       type: 'checkbox',
       message: 'What languages are included in this project?',
-      name: 'Technologies',
+      name: 'technologies',
       choices: ['HTML', 'CSS', 'JavaScript', 'SQL']
     }
-  ])
-
-
+  ];
+const tableOfContents = [
+    'Title',
+    'Description',
+    'Installation',
+    'Usage',
+    'License',
+    'Contributing',
+    'Test',
+    'Contact',
+    'Technologies',
+]
+tableOfContents.forEach((item, index) => {
+    questions[index].name = item
+})
 // TODO: Create a function to write README file
-fs.writeFile('../readme-guide.md', generatePage(fileName, data), err => {
-    if (err) throw new Error(err);
+.then(function(data) {
+    const filename =
+      data.name
+        .toLowerCase()
+        .split(' ')
+        .join('') + '.json';
 
+    fs.writeFile(filename, JSON.stringify(data, null, '\t'), function(err) {
+      if (err) {
+        return console.log(err);
+      }
+    });
+  });
+
+fs.writeFile('README.md', `${title}`, generatePage(filename, data), err => {
+    if (err) throw new Error(err);
+});
 // TODO: Create a function to initialize app
 function init() {}
 
